2d57382233ad28603ce06e3da9b90b0f9ad6864f
nollid08 <dillonkdy@gmail.com>
2024-10-27 23:20:20 +0000

    Fully Fixed Flickering

--- lib/providers/resource_calendar_source_provider.dart
+++ lib/providers/resource_calendar_source_provider.dart
@@ -1,4 +1,6 @@
+import 'package:dashboard/models/coach.dart';
 import 'package:dashboard/models/resource_calendar_source.dart';
+import 'package:dashboard/models/session.dart';
 import 'package:dashboard/providers/coaches_provider.dart';
 import 'package:dashboard/providers/sessions_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -9,7 +11,9 @@ part 'resource_calendar_source_provider.g.dart';
 Stream<ResourceCalendarDataSource> resourceCalendarSource(
     ResourceCalendarSourceRef ref) async* {
   final coaches = await ref.watch(coachesProvider.future);
-  final sessions = await ref.watch(sessionsProvider().future);
+  final sessions = await ref.read(sessionsProvider().future);
+  // final List<Coach> coaches = [];
+  // final List<Session> sessions = [];
 
   yield ResourceCalendarDataSource.fromData(
     sessions: sessions,
