b4e085cb775a9ec910fab282e8ca7d2e5d2fa804
nollid08 <dillonkdy@gmail.com>
2024-10-31 22:28:27 +0000

    Email Functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,10 @@ import { find_sessions_available_coaches_function } from "./functions/find_sessi
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { find_available_booking_options_function } from "./functions/find_available_booking_options";
+import { getAuth } from "firebase-admin/auth";
+import { defineString } from "firebase-functions/params";
+
+const brevoApiKey = defineString("BREVO_API_KEY");
 
 initializeApp();
 
@@ -45,4 +49,68 @@ export const normalise_client_data = fbTriggers
       });
     });
     return Promise.all([p1, q2]);
+  });
+
+export const equipment_transfer_notification = fbTriggers
+  .document("logs/{logId}")
+  .onCreate(async (docSnapshot, context) => {
+    const recipientUid: string = docSnapshot.data().recipientUid;
+    const origineeUid: string = docSnapshot.data().origineeUid;
+    const quantityTransferred: Number = docSnapshot.data().quantityTransferred;
+    const equipmentId: string = docSnapshot.data().equipmentId;
+
+    const auth = getAuth();
+    const recipient = await auth.getUser(recipientUid);
+    const originee = await auth.getUser(origineeUid);
+
+    const db = getFirestore();
+    const eqiupment = await db.collection("equipment").doc(equipmentId).get();
+
+    console.log(
+      `Equipment ${eqiupment?.data()?.name} has been transferred from ${
+        originee.displayName
+      } to ${recipient.displayName} with quantity ${quantityTransferred}`
+    );
+    // load from environment variables
+    const url = "https://api.brevo.com/v3/smtp/email";
+
+    console.log("Email Info:", recipient.email);
+    const data = {
+      to: [
+        {
+          email: recipient.email,
+          name: recipient.displayName,
+        },
+      ],
+      templateId: 796,
+      params: {
+        SENDER: originee.displayName,
+        NAME: eqiupment?.data()?.name,
+        QUANTITY: quantityTransferred.toString(),
+      },
+
+      headers: {
+        "X-Mailin-custom":
+          "custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
+        charset: "iso-8859-1",
+      },
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        "api-key": brevoApiKey.value(),
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${response.status} - ${errorData.message}`);
+    }
+
+    const result = await response.json();
+    console.log("Email sent successfully:", result);
   });
